c3e4e51642644dd8db380bb778c5bef9d27e742c
Adrien Martinaud <drop_m37@hotmail.com>
2024-02-17 15:37:09 +0100

    Installation de react app et react router. Mise en place des composants Header et Hero

---
+++ front-end/src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import "./header.css";
+import logo from "../../assets/img/argentBankLogo.png";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav className="main-nav">
+      <Link to="/" className="main-nav-logo">
+        <img
+          className="main-nav-logo-image"
+          src={logo}
+          alt="Argent Bank Logo"
+        />
+      </Link>
+      <div>
+        <NavLink className="main-nav-item" href="./sign-in.html">
+          <i class="fa fa-user-circle"></i>
+          Sign In
+        </NavLink>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
